0be62bbfdad1fc93cd37ce87ec68e0fa2e120268
evairx <xxbrazzersxx35@gmail.com>
2024-03-21 21:03:34 -0300

    Refactor code in calendar.js

---
+++ src/components/classItem.js
@@ -0,0 +1,62 @@
+import React, { useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Animated, { useSharedValue, withTiming, useAnimatedStyle } from 'react-native-reanimated';
+
+const ClaseItem = ({ classItem, index }) => {
+  const translateY = useSharedValue(100);
+  const opacity = useSharedValue(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      translateY.value = withTiming(0, { duration: 500 });
+      opacity.value = withTiming(1, { duration: 500 });
+    }, index * 500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateY: translateY.value }],
+      opacity: opacity.value,
+    };
+  });
+
+  return (
+    <Animated.View style={[styles.classItem, animatedStyle]}>
+      <Text style={styles.hour}>{classItem.horaInicio} a {classItem.horaFin}</Text>
+      <Text style={styles.className}>{classItem.nombre}</Text>
+      <Text style={styles.subText}>{classItem.seccion}</Text>
+      <Text style={styles.subText}>Profesor: {classItem.profesor}</Text>
+      <Text style={styles.subText}>Sala: {classItem.sala}</Text>
+      <Text style={styles.subText}>Sede: {classItem.sede}</Text>
+    </Animated.View>
+  );
+};
+
+const styles = StyleSheet.create({
+  classItem: {
+    borderLeftColor: '#012C56',
+    borderLeftWidth: 5,
+    borderBottomColor: '#012C56',
+    borderBottomWidth: 2,
+    marginTop: 7,
+    marginBottom: 7,
+    width: '90%',
+    padding: 10,
+    borderRadius: 10,
+  },
+  hour: {
+    fontSize: 12,
+    fontWeight: '900',
+  },
+  className: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  subText: {
+    fontSize: 11,
+    paddingVertical: 2,
+  },
+});
+
+export default ClaseItem;
--- src/screens/calendar.js
+++ src/screens/calendar.js
@@ -1,10 +1,11 @@
-import { useState, useEffect } from 'react';
-import { ActivityIndicator, View ,ScrollView, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { ActivityIndicator, View, ScrollView, Text, StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { useNavigation } from '@react-navigation/native';
 import * as FileSystem from 'expo-file-system';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import BackToHome from '../components/backToHome';
+import ClaseItem from '../components/classItem';
 import Calendary from '../components/calendary';
 import Dropdown from '../components/dropdown';
 import Nav from '../components/nav';
@@ -19,49 +20,49 @@ const Calendar = () => {
   const navigation = useNavigation();
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const token = await AsyncStorage.getItem('access_token');
-        const id = await AsyncStorage.getItem('idAlumno');
-
-        if (token && id) {
-          const urlCalendary = process.env.EXPO_PUBLIC_CALENDAR_ENDPOINT;
-
-          const response = await fetch(`${urlCalendary}=${id}`,  {
-            method: 'GET',
-            headers: {
-              Authorization: `Bearer ${token}`
-            }
-          });
-
-          if (response.ok) {
-            const jsonData = await response.json();
-            setHorario(jsonData[0]);
-            const directory = FileSystem.documentDirectory + 'data/';
-            console.log(directory)
-            const fileName = 'horario.json';
-            const filePath = directory + fileName;
-            await FileSystem.makeDirectoryAsync(directory, { intermediates: true });
-            await FileSystem.writeAsStringAsync(filePath, JSON.stringify(jsonData));
-          } else {
-            console.error('Error al obtener los datos del alumno:', response.status);
-          }
-        }
-      } catch (error) {
-        console.error('Error al realizar la solicitud:', error);
-        setError(true)
-        await AsyncStorage.removeItem('access_token');
-        await AsyncStorage.removeItem('idAlumno');
-        await AsyncStorage.removeItem('codAlumno');
-        await AsyncStorage.removeItem('rut');
-        navigation.navigate('Login');
-      } finally {
-        setLoading(false);
-      }
-    };
-
     fetchData();
   }, []);
+
+  const fetchData = async () => {
+    try {
+      const token = await AsyncStorage.getItem('access_token');
+      const id = await AsyncStorage.getItem('idAlumno');
+
+      if (token && id) {
+        const urlCalendary = process.env.EXPO_PUBLIC_CALENDAR_ENDPOINT;
+
+        const response = await fetch(`${urlCalendary}=${id}`, {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+
+        if (response.ok) {
+          const jsonData = await response.json();
+          setHorario(jsonData[0]);
+          const directory = FileSystem.documentDirectory + 'data/';
+          console.log(directory)
+          const fileName = 'horario.json';
+          const filePath = directory + fileName;
+          await FileSystem.makeDirectoryAsync(directory, { intermediates: true });
+          await FileSystem.writeAsStringAsync(filePath, JSON.stringify(jsonData));
+        } else {
+          console.error('Error al obtener los datos del alumno:', response.status);
+        }
+      }
+    } catch (error) {
+      console.error('Error al realizar la solicitud:', error);
+      setError(true)
+      await AsyncStorage.removeItem('access_token');
+      await AsyncStorage.removeItem('idAlumno');
+      await AsyncStorage.removeItem('codAlumno');
+      await AsyncStorage.removeItem('rut');
+      navigation.navigate('Login');
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     const today = new Date();
@@ -93,7 +94,7 @@ const Calendar = () => {
 
   return (
     <View style={styles.container}>
-      <StatusBar style="dark"/>
+      <StatusBar style="dark" />
       <View style={styles.header}>
         <BackToHome style={styles.backText}>Horario</BackToHome>
         {horario && horario.nomCarrera && <Dropdown options={options} selectedValue={selectedDay} onValueChange={handleDropdownChange} />}
@@ -102,7 +103,7 @@ const Calendar = () => {
         <ScrollView contentContainerStyle={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           <Text style={styles.errorText}>No se ha podido conectar con el servidor de DuocUc. Por favor, inténtelo de nuevo más tarde.</Text>
         </ScrollView>
-        ) : (
+      ) : (
         loading ? (
           <ScrollView contentContainerStyle={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <ActivityIndicator size="large" color="rgb(252, 189, 27)" />
@@ -116,14 +117,7 @@ const Calendar = () => {
                 <View style={styles.classesContainer}>
                   {classes.length > 0 ? (
                     classes.map((classItem, index) => (
-                      <View key={index} style={styles.classItem}>
-                        <Text style={styles.hour}>{classItem.horaInicio} a {classItem.horaFin}</Text>
-                        <Text style={styles.className}>{classItem.nombre}</Text>
-                        <Text style={styles.subText}>{classItem.seccion}</Text>
-                        <Text style={styles.subText}>Profesor: {classItem.profesor}</Text>
-                        <Text style={styles.subText}>Sala: {classItem.sala}</Text>
-                        <Text style={styles.subText}>Sede: {classItem.sede}</Text>
-                      </View>
+                      <ClaseItem key={index} classItem={classItem} />
                     ))
                   ) : (
                     <View style={styles.noClasses}>
@@ -172,29 +166,6 @@ const styles = StyleSheet.create({
   classesContainer: {
     paddingHorizontal: 5,
     paddingVertical: 5,
-  },
-  classItem: {
-    borderLeftColor: '#012C56',
-    borderLeftWidth: 5,
-    borderBottomColor: '#012C56',
-    borderBottomWidth: 2,
-    marginTop: 7,
-    marginBottom: 7,
-    width: '90%',
-    padding: 10,
-    borderRadius: 10,
-  },
-  hour: {
-    fontSize: 12,
-    fontWeight: '900',
-  },
-  className: { 
-    fontSize: 14,
-    fontWeight: 'bold',
-  },
-  subText: {
-    fontSize: 11,
-    paddingVertical: 2,
   },
   noClasses: {
     display: 'flex',
