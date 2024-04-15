1fc9f27d8b36536b9001c22bfba05269af2a0b6c
Phownix <paulogutierrez211@gmail.com>
2024-04-15 01:00:27 -0400

    I'm Here

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import Navigation from "./src/navigation";
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
+  console.log("Phownix was here!")
   return (
     <NavigationContainer>
         <Navigation/>
